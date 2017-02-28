3c0434d051e1eee9fbbb6144b24e33db6840da4c
arjunchakkalakunnel <arjun.mohan@ults.in>
2017-02-28 11:21:14 +0530

    adding UnitTestCase 	new file:   app/calculator.py 	modified:   app/tests.py 	modified:   manage.py

---
+++ app/calculator.py
@@ -0,0 +1,8 @@
+class Calculator(object):
+ 
+    def add(self, x, y):
+    	number_type=(int, long, float, complex)
+    	if isinstance(x,number_type)and isinstance(y,number_type):
+        	return x+y
+        else:
+        	raise ValueError  	
